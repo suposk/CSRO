f847190df3dd1707035e0b31f97a2f77a02b6479
suposk <jan.supolik@gmail.com>
2020-12-17 12:54:41 +0100

    problem with autorize view

--- src/CSRO.Client.Blazor.WebApp/Program.cs
+++ src/CSRO.Client.Blazor.WebApp/Program.cs
@@ -1,6 +1,10 @@
+using Azure.Identity;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,11 +22,35 @@ namespace CSRO.Client.Blazor.WebApp
             CreateHostBuilder(args).Build().Run();
         }
 
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+                    bool UseKeyVault = builtConfig.GetValue<bool>("UseKeyVault");
+                    if (UseKeyVault)
+                    {
+                        var vaultName = builtConfig["VaultName"];
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        config.AddAzureKeyVault(
+                            vaultName,
+                            keyVaultClient,
+                            new DefaultKeyVaultSecretManager());
+                    }
                 });
     }
 }
--- src/CSRO.Client.Blazor.WebApp/Startup.cs
+++ src/CSRO.Client.Blazor.WebApp/Startup.cs
@@ -1,11 +1,17 @@
 using CSRO.Client.Blazor.WebApp.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using MudBlazor;
 using MudBlazor.Services;
 using System;
@@ -28,14 +34,83 @@ namespace CSRO.Client.Blazor.WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
+            //{
+            //    cacheOptions.ContainerName = Configuration["CosmosCache:ContainerName"];
+            //    cacheOptions.DatabaseName = Configuration["CosmosCache:DatabaseName"];
+            //    cacheOptions.ClientBuilder = new CosmosClientBuilder(Configuration["CosmosCache:ConnectionString"]);
+            //    cacheOptions.CreateIfNotExists = true;
+            //});                       
+
+            bool UseKeyVault = Configuration.GetValue<bool>("UseKeyVault");
+            var VaultName = Configuration.GetValue<string>("VaultName");
+            string clientSecret = null;
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            if (UseKeyVault)
+            {
+                try
+                {
+                    var janoSetting = keyVaultClient.GetSecretAsync(VaultName, "JanoSetting").Result.Value;
+                    Console.WriteLine($"JanoSetting from Vault: {janoSetting}");
+
+                    clientSecret = keyVaultClient.GetSecretAsync(VaultName, "ClientSecret").Result.Value;
+                    Console.WriteLine($"ClientSecret first 3 char from Vault: {clientSecret?.Substring(startIndex: 0, length: 3)}");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            string ApiEndpoint = Configuration.GetValue<string>("ApiEndpoint");
+            services.AddHttpClient("api", (client) =>
+            {
+                client.BaseAddress = new Uri(ApiEndpoint);
+            });
+
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddInMemoryTokenCaches()
+                ;
+
+            services.Configure<MicrosoftIdentityOptions>(options =>
+            {
+                options.ResponseType = OpenIdConnectResponseType.Code;
+
+                if (UseKeyVault)
+                    options.ClientSecret = clientSecret;
+
+            });
+
+            services.AddControllersWithViews()
+                .AddMicrosoftIdentityUI();
+
+            services.AddAuthorization(options =>
+            {
+                // By default, all incoming requests will be authorized according to the default policy
+                //Will automatical sign in user
+                //options.FallbackPolicy = options.DefaultPolicy;
+            });
+
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor()
+                .AddMicrosoftIdentityConsentHandler();
+
+            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<ISampleService, SampleService>();
+            //services.AddScoped<IVersionService, VersionService>();
+
+            var jano = Configuration.GetValue<string>("JanoSetting");
+            Console.WriteLine($"Configuration JanoSetting: {jano}");
+
+            var sec = Configuration.GetValue<string>("AzureAd:ClientSecret");
+            Console.WriteLine($"Configuration AzureAd:ClientSecret first 3 char: {sec.Substring(startIndex: 0, length: 3)}");
 
             services.AddMudBlazorDialog();
             services.AddMudBlazorSnackbar();
             services.AddMudBlazorResizeListener();
-
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,8 +132,12 @@ namespace CSRO.Client.Blazor.WebApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
