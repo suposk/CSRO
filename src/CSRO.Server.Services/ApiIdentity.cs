2c35dcbd01c1f5d0efd15cd2861ae751c20f8528
suposk <jan.supolik@gmail.com>
2021-01-04 22:47:13 +0100

    using name

--- src/CSRO.Server.Services/ApiIdentity.cs
+++ src/CSRO.Server.Services/ApiIdentity.cs
@@ -22,6 +22,23 @@ namespace CSRO.Server.Services
         //}
         public string GetUserName()
         {
+            List<string> list = new List<string>();
+            var nameId = _context.HttpContext?.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier);
+            var email = _context.HttpContext?.User.FindFirst(c => c.Type == ClaimTypes.Email);
+            var upn = _context.HttpContext?.User.FindFirst(c => c.Type == ClaimTypes.WindowsAccountName);
+            var userData = _context.HttpContext?.User.FindFirst(c => c.Type == ClaimTypes.UserData);
+            foreach(var item in _context.HttpContext?.User.Claims)
+            {
+                if (item.Type.ToLower() == "name")
+                    list.Add(item.Value);
+            }
+            if (list.Count == 0 && email != null && !string.IsNullOrWhiteSpace(email.Value))
+                return email.Value;
+            if (list.Count > 0)
+            {
+                var shortestName = list.OrderBy(name => name.Length).FirstOrDefault();
+                return shortestName;
+            }                
             return _context.HttpContext?.User?.Identity?.Name;
         }
 
