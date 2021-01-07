67b2d0ebffdc72e1360c135928ae499b72bebaad
suposk <jan.supolik@gmail.com>
2021-01-07 13:32:17 +0100

    added restart VM Form, also data service

---
+++ src/CSRO.Client.Services/VmDataService .cs
@@ -0,0 +1,159 @@
+﻿using AutoMapper;
+using CSRO.Client.Services;
+using CSRO.Client.Services.Dtos;
+using CSRO.Client.Services.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CSRO.Client.Services
+{
+    public class VmDataService : BaseDataService, IBaseDataService<Vm>
+    {
+        public VmDataService(IHttpClientFactory httpClientFactory, IAuthCsroService authCsroService, IMapper mapper, 
+            IConfiguration configuration)
+            : base(httpClientFactory, authCsroService, mapper, configuration)
+        {
+            ApiPart = "api/vm/";
+            //Scope = "api://ee2f0320-29c3-432a-bf84-a5d4277ce052/user_impersonation";
+            Scope = Configuration.GetValue<string>("Scope_Api");
+            ClientName = "api";
+
+            base.Init();
+        }
+
+        public async Task<Vm> AddItemAsync(Vm item)
+        {
+            try
+            {
+                await base.AddAuthHeaderAsync();
+
+                var url = $"{ApiPart}";
+                var add = Mapper.Map<TicketDto>(item);
+                var httpcontent = new StringContent(JsonSerializer.Serialize(add, _options), Encoding.UTF8, "application/json");
+                var apiData = await HttpClientBase.PostAsync(url, httpcontent).ConfigureAwait(false);
+
+                if (apiData.IsSuccessStatusCode)
+                {
+                    var content = await apiData.Content.ReadAsStringAsync();
+                    var ser = JsonSerializer.Deserialize<TicketDto>(content, _options);
+                    var result = Mapper.Map<Vm>(ser);
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.HandleException(ex);
+            }
+            return null;
+        }
+
+        public async Task<bool> UpdateItemAsync(Vm item)
+        {
+            try
+            {
+                await base.AddAuthHeaderAsync();
+
+                var url = $"{ApiPart}";
+                var add = Mapper.Map<TicketDto>(item);
+                var httpcontent = new StringContent(JsonSerializer.Serialize(add, _options), Encoding.UTF8, "application/json");
+                var apiData = await HttpClientBase.PutAsync(url, httpcontent).ConfigureAwait(false);
+
+                if (apiData.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.HandleException(ex);
+            }
+            return false;
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            try
+            {
+                await base.AddAuthHeaderAsync();
+
+                var url = $"{ApiPart}{id}";
+                var apiData = await HttpClientBase.DeleteAsync(url).ConfigureAwait(false);
+
+                if (apiData.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.HandleException(ex);
+            }
+            return false;
+        }
+
+        public async Task<Vm> GetItemByIdAsync(int id)
+        {
+            try
+            {
+                await base.AddAuthHeaderAsync();
+
+                var url = $"{ApiPart}{id}";
+                var apiData = await HttpClientBase.GetAsync(url).ConfigureAwait(false);
+
+                if (apiData.IsSuccessStatusCode)
+                {
+                    var content = await apiData.Content.ReadAsStringAsync();
+                    var ser = JsonSerializer.Deserialize<TicketDto>(content, _options);
+                    var result = Mapper.Map<Vm>(ser);
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.HandleException(ex);
+            }
+            return null;
+        }
+
+        public async Task<List<Vm>> GetItemsAsync()
+        {
+            try
+            {
+                await base.AddAuthHeaderAsync();
+
+                var url = $"{ApiPart}";
+                var apiData = await HttpClientBase.GetAsync(url).ConfigureAwait(false);
+
+                if (apiData.IsSuccessStatusCode)
+                {
+                    var content = await apiData.Content.ReadAsStringAsync();
+                    var ser = JsonSerializer.Deserialize<List<TicketDto>>(content, _options);
+                    var result = Mapper.Map<List<Vm>>(ser);
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.HandleException(ex);
+            }
+            return null;
+        }
+
+        public Task<List<Vm>> GetItemsByParrentIdAsync(int parrentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Vm>> GetItemsByTypeAsync(string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
