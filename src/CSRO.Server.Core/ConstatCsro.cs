0d635c210369c117bf4da3e1ee55741e97df280d
suposk <jan.supolik@gmail.com>
2021-01-11 10:48:59 +0100

    added CSRO.Server.Core

---
+++ src/CSRO.Server.Core/ConstatCsro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSRO.Server.Core
+{
+    public static class ConstatCsro
+    {
+        public static class ClientNames
+        {
+            /// <summary>
+            /// Endpoint and ClientName same value
+            /// </summary>
+            public const string MANAGEMENT_AZURE_EndPoint = "https://management.azure.com";
+        }
+
+        public static class Scopes
+        {
+            public const string MANAGEMENT_AZURE_SCOPE = "https://management.azure.com//.default";
+        }
+    }
+}
