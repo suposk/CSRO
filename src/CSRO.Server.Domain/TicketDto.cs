4778198f435a32cb1f6da1e53c76f96578c0b37d
suposk <jan.supolik@gmail.com>
2021-01-13 13:26:08 +0100

    added attrubutes for validation

--- src/CSRO.Server.Domain/TicketDto.cs
+++ src/CSRO.Server.Domain/TicketDto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSRO.Server.Domain
 {
     public class TicketDto : DtoBase
     {
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string RequestedFor { get; set; }
 
         public bool IsOnBehalf { get; set; }
