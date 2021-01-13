b170ff6b9b01eb75b81fae2517a13d5907976c86
suposk <jan.supolik@gmail.com>
2021-01-13 16:42:31 +0100

    async test val method

--- src/CSRO.Client.Services/Validation/VmTicketValidator.cs
+++ src/CSRO.Client.Services/Validation/VmTicketValidator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSRO.Client.Services.Validation
@@ -13,9 +14,23 @@ namespace CSRO.Client.Services.Validation
     {
         public VmTicketValidator()
         {
-            RuleFor(p => p.VmName).NotEmpty();
+            RuleFor(p => p.VmName).
+                NotEmpty()
+                .MustAsync(ValidateVm).WithMessage("Test only, name has to contain vm letter");                
+
             RuleFor(p => p.SubcriptionId).NotEmpty();
             RuleFor(p => p.ResorceGroup).NotEmpty();
+        }
+
+        private async Task<bool> ValidateVm(string vm, CancellationToken token)
+        {
+            await Task.Delay(1 * 1000, token).ConfigureAwait(false);
+            if (token.IsCancellationRequested)
+                return false;
+
+            if (vm == null)
+                return false;
+            return vm.ToLower().Contains("vm") ? true : false;
         }
     }
 }
