ed4caba404a7a3a4a76e27832ed5769d6af48ffa
suposk <jan.supolik@gmail.com>
2021-02-17 11:50:51 +0100

    IGsnowService

--- src/CSRO.Common.AzureSdkServices/FakeGsnowService.cs
+++ src/CSRO.Common.AzureSdkServices/FakeGsnowService.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSRO.Common.AzureSdkServices
 {
     public interface IGsnowService
     {
-
+        Task<bool> IsValidGsnowTicket(string gsnowTicketNumber, CancellationToken cancelToken = default);
     }
 
     public class FakeGsnowService : IGsnowService
@@ -23,7 +24,7 @@ namespace CSRO.Common.AzureSdkServices
             _tokenCredential = _csroTokenCredentialProvider.GetCredential();
         }
 
-        public async Task<bool> IsValidGsnowTicket(string gsnowTicketNumber)
+        public async Task<bool> IsValidGsnowTicket(string gsnowTicketNumber, CancellationToken cancelToken = default)
         {
             if (string.IsNullOrWhiteSpace(gsnowTicketNumber))            
                 throw new ArgumentException($"'{nameof(gsnowTicketNumber)}' cannot be null or whitespace", nameof(gsnowTicketNumber));            
