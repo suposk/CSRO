7f7a8b74d565277c4110096084504b60eab72b61
suposk <jan.supolik@gmail.com>
2021-03-26 13:21:03 +0100

    CustomerCodes

---
+++ src/CSRO.Client.Blazor.WebApp/Pages/Customers/CustomerCodesBase.cs
@@ -0,0 +1,12 @@
+﻿using CSRO.Client.Blazor.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSRO.Client.Blazor.WebApp.Pages.Customers
+{
+    public class CustomerCodesBase : CsroComponentBase
+    {
+    }
+}
