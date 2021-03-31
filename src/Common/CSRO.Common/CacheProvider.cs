473fc27f6473b6d3ca74192cb143f0be06bfa04b
suposk <jan.supolik@gmail.com>
2021-03-31 09:47:44 +0200

    CacheProvider new methods

--- src/Common/CSRO.Common/CacheProvider.cs
+++ src/Common/CSRO.Common/CacheProvider.cs
@@ -7,15 +7,6 @@ using System.Threading.Tasks;
 
 namespace CSRO.Common
 {
-    public interface ICacheProvider
-    {
-        const int CacheSeconds = 300; //5 min
-
-        T GetFromCache<T>(string key) where T : class;
-        void SetCache<T>(string key, T value) where T : class;
-        void SetCache<T>(string key, T value, int seconds = CacheSeconds) where T : class;
-        void ClearCache(string key);
-    }
     public class CacheProvider : ICacheProvider
     {        
         private readonly IMemoryCache _cache;
@@ -37,13 +28,37 @@ namespace CSRO.Common
         }
 
         public void SetCache<T>(string key, T value, int seconds = ICacheProvider.CacheSeconds) where T : class
-        {
+        {         
             _cache.Set(key, value, DateTimeOffset.Now.AddSeconds(seconds));
+        }
+
+        public void SetCache<T>(string key, string id, T value) where T : class
+        {
+            if (string.IsNullOrWhiteSpace(id))            
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));            
+
+            SetCache($"{key}-{id}", value);
+        }
+
+        public void SetCache<T>(string key, string id, T value, int seconds = ICacheProvider.CacheSeconds) where T : class
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
+
+            SetCache($"{key}-{id}", value, seconds);
         }
 
         public void ClearCache(string key)
         {
             _cache.Remove(key);
+        }
+
+        public void ClearCache(string key, string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException($"'{nameof(id)}' cannot be null or whitespace.", nameof(id));
+
+            _cache.Remove($"{key}-{id}");
         }
     }
 }
