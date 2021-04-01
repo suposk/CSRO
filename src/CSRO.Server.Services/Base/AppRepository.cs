f255f72af41d2b45016eff71b57ba79c1b9413dd
suposk <jan.supolik@gmail.com>
2021-04-01 09:25:06 +0200

    generic for app contexct

---
+++ src/CSRO.Server.Services/Base/AppRepository.cs
@@ -0,0 +1,18 @@
+﻿using CSRO.Server.Entities;
+using CSRO.Server.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSRO.Server.Services.Base
+{
+    public class AppRepository<TModel> : Repository<TModel>, IRepository<TModel> where TModel : EntityBase
+    {
+        public AppRepository(AppVersionContext context, IApiIdentity apiIdentity) : base(context, apiIdentity)
+        {
+
+        }
+    }
+}
