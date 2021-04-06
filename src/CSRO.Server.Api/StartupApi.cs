756484cc01f26bbb5dde415a04d4da84ca132eee
suposk <jan.supolik@gmail.com>
2021-04-06 11:36:36 +0200

    coments

--- src/CSRO.Server.Api/StartupApi.cs
+++ src/CSRO.Server.Api/StartupApi.cs
@@ -256,40 +256,6 @@ namespace CSRO.Server.Api
             services.AddScoped<IVmTicketRepository, VmTicketRepository>();            
             services.AddScoped<IAtCodecmdbReferenceRepository, AtCodecmdbReferenceRepository>();
 
-            //var serviceProvider = services.BuildServiceProvider();
-
-            //services.AddScoped<IRepository<AppVersion>>(sp =>
-            //{
-            //    var apiIdentity = serviceProvider.GetService<IApiIdentity>();
-            //    var ctx = serviceProvider.GetService<AppVersionContext>();
-            //    IRepository<AppVersion> obj = new Repository<AppVersion>(ctx, apiIdentity);
-            //    return obj;
-            //});
-
-            //services.AddScoped<IRepository<Ticket>>(sp =>
-            //{
-            //    var apiIdentity = serviceProvider.GetService<IApiIdentity>();
-            //    var ctx = serviceProvider.GetService<AppVersionContext>();
-            //    IRepository<Ticket> obj = new Repository<Ticket>(ctx, apiIdentity);
-            //    return obj;
-            //});
-
-            //services.AddScoped<IRepository<VmTicket>>(sp =>
-            //{
-            //    var apiIdentity = serviceProvider.GetService<IApiIdentity>();
-            //    var ctx = serviceProvider.GetService<AppVersionContext>();
-            //    IRepository<VmTicket> obj = new Repository<VmTicket>(ctx, apiIdentity);
-            //    return obj;
-            //});
-
-            //services.AddScoped<IRepository<AtCodecmdbReference>>(sp =>
-            //{
-            //    var apiIdentity = serviceProvider.GetService<IApiIdentity>();
-            //    var ctx = serviceProvider.GetService<AppVersionContext>();
-            //    IRepository<AtCodecmdbReference> obj = new Repository<AtCodecmdbReference>(ctx, apiIdentity);
-            //    return obj;
-            //});
-
             #endregion
         }
 
