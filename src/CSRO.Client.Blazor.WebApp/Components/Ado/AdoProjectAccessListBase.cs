c13ea82f6f0e84d9409048643ca73ef0d9341892
suposk <jan.supolik@gmail.com>
2021-04-08 10:49:18 +0200

    removed modified

---
+++ src/CSRO.Client.Blazor.WebApp/Components/Ado/AdoProjectAccessListBase.cs
@@ -0,0 +1,12 @@
+﻿using CSRO.Client.Blazor.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSRO.Client.Blazor.WebApp.Components.Ado
+{
+    public class AdoProjectAccessListBase : CsroComponentBase
+    {
+    }
+}
