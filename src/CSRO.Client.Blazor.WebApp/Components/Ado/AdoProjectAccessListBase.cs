1786ad0d5f87492bfbff7516be1fac410839c272
suposk <jan.supolik@gmail.com>
2021-04-08 11:07:09 +0200

    load all tickets

--- src/CSRO.Client.Blazor.WebApp/Components/Ado/AdoProjectAccessListBase.cs
+++ src/CSRO.Client.Blazor.WebApp/Components/Ado/AdoProjectAccessListBase.cs
@@ -1,4 +1,13 @@
 ﻿using CSRO.Client.Blazor.UI;
+using CSRO.Client.Blazor.UI.Services;
+using CSRO.Client.Blazor.WebApp.Components;
+using CSRO.Client.Services;
+using CSRO.Client.Services.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
+using MudBlazor;
+using MudBlazor.Dialog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +17,53 @@ namespace CSRO.Client.Blazor.WebApp.Components.Ado
 {
     public class AdoProjectAccessListBase : CsroComponentBase
     {
+        #region Params and Injects
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ICsroDialogService CsroDialogService { get; set; }
+
+        [Inject]
+        public IAdoProjectAccessDataService AdoProjectAccessDataService { get; set; }
+
+        [Inject]
+        public ILogger<AdoProjectAccessListBase> Logger { get; set; }
+
+        #endregion
+
+        public List<AdoProjectAccessModel> Requests { get; set; } = new List<AdoProjectAccessModel>();
+
+        protected async override Task OnInitializedAsync()
+        {
+
+            try
+            {
+                ShowLoading();
+                Requests.Clear();
+                Requests = await AdoProjectAccessDataService.GetItemsAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, nameof(OnInitializedAsync));
+                await CsroDialogService.ShowError("Error", ex?.Message);
+            }
+            HideLoading();
+        }
+
+        public async Task DeleteTicketAsync(AdoProjectAccessModel ticket)
+        {
+            var ok = await CsroDialogService.ShowDialog("Delete Ticket", $"Do you really want to delete these record Id: {ticket.Id}?", "Delete");
+            if (ok)
+            {
+                var res = await AdoProjectAccessDataService.DeleteItemAsync(ticket.Id);
+                if (res)
+                {
+                    Requests.Remove(ticket);
+                }
+            }
+        }
+
     }
 }
