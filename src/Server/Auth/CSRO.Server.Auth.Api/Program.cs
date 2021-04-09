4d160dfc0b271eb116432b6be56e1cfa39de4204
suposk <jan.supolik@gmail.com>
2021-04-09 16:55:59 +0200

    StartupAuthApi mod

--- src/Server/Auth/CSRO.Server.Auth.Api/Program.cs
+++ src/Server/Auth/CSRO.Server.Auth.Api/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSRO.Server.Entities;
 
 namespace CSRO.Server.Auth.Api
 {
@@ -13,7 +16,41 @@ namespace CSRO.Server.Auth.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            // migrate the database.  Best practice = in Main, using service scope
+            using (var scope = host.Services.CreateScope())
+            {
+                ILogger<Program> logger = null;
+                UserContext context = null;
+
+                try
+                {
+                    logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                }
+                catch { }
+
+                try
+                {
+                    logger?.LogInformation($"CreateHostBuilder Started {nameof(UserContext)} ");
+
+                    context = scope.ServiceProvider.GetService<UserContext>();
+
+                    // for demo purposes, delete the database & migrate on startup so we can start with a clean slate                   
+                    //context.Database.EnsureDeleted(); logger?.LogInformation("Called EnsureDeleted");
+                    //context?.Database.EnsureCreated(); logger?.LogInformation("Called EnsureCreated");
+                    context?.Database.Migrate(); logger?.LogInformation("Called Migrate");
+                }
+                catch (Exception ex)
+                {
+                    logger?.LogError(ex, $"An error occurred while migrating the database {nameof(UserContext)}.");
+                }
+
+                // run the web app
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- src/Server/Auth/CSRO.Server.Auth.Api/StartupAuthApi.cs
+++ src/Server/Auth/CSRO.Server.Auth.Api/StartupAuthApi.cs
@@ -1,37 +1,161 @@
+using AutoMapper;
+using CSRO.Server.Entities;
+using CSRO.Server.Entities.Entity;
+using CSRO.Server.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
+using System.Net.Http;
+using System.Net;
+using CSRO.Server.Core.Helpers;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using CSRO.Common;
+using CSRO.Server.Services.Utils;
+using CSRO.Server.Services.Ado;
+using CSRO.Server.Infrastructure.Search;
+using System.Reflection;
+using MediatR;
+using CSRO.Server.Infrastructure.MessageBus;
+using CSRO.Server.Services;
+using Microsoft.AspNetCore.Authentication;
+using CSRO.Server.Core;
+using CSRO.Server.Auth.Api.Services;
 
 namespace CSRO.Server.Auth.Api
 {
     public class StartupAuthApi
     {
-        public StartupAuthApi(IConfiguration configuration)
+        public StartupAuthApi(
+            IConfiguration configuration,
+            IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
+            var myType = typeof(StartupAuthApi);
+            _namespace = myType.Namespace;
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Information);
+                builder.AddConsole();
+                builder.AddEventSourceLogger();
+            });
+            _logger = loggerFactory.CreateLogger(nameof(StartupAuthApi));
+            _logger.LogInformation($"Created {nameof(StartupAuthApi)} _logger");
         }
 
         public IConfiguration Configuration { get; }
+        private readonly ILogger _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly string _namespace = null;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var azureAdOptions = Configuration.GetSection(nameof(AzureAd)).Get<AzureAd>();
+
+            string ClientSecret = null;
+            string SqlConnString = Configuration.GetConnectionString("SqlConnString");
+            string TokenCacheDbConnStr = Configuration.GetConnectionString("TokenCacheDbConnStr");
+            string ClientSecretVaultName = Configuration.GetValue<string>("ClientSecretVaultName");
+
+            bool UseKeyVault = Configuration.GetValue<bool>("UseKeyVault");
+            if (UseKeyVault)
+            {
+                try
+                {
+                    var VaultName = Configuration.GetValue<string>("CsroVaultNeuDev");
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    ClientSecret = keyVaultClient.GetSecretAsync(VaultName, ClientSecretVaultName).Result.Value;
+                    azureAdOptions.ClientSecret = ClientSecret;
+                    Configuration["AzureAd:ClientSecret"] = ClientSecret;
+
+                    var SqlConnStringVault = keyVaultClient.GetSecretAsync(VaultName, "SqlConnStringVault").Result.Value;
+                    SqlConnString = SqlConnStringVault;
+                    Configuration["ConnectionStrings:SqlConnString"] = SqlConnString;
+
+                    var TokenCacheDbConnStrVault = keyVaultClient.GetSecretAsync(VaultName, "TokenCacheDbConnStrVault").Result.Value;
+                    TokenCacheDbConnStr = TokenCacheDbConnStrVault;
+                    Configuration["ConnectionStrings:TokenCacheDbConnStr"] = TokenCacheDbConnStr;
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogError("Error reading Keyvalut", ex);
+                }
+            }
+
+            #region Distributed Token Caches
+
+            //services.AddCosmosCache((CosmosCacheOptions cacheOptions) =>
+            //{
+            //    cacheOptions.ContainerName = Configuration["CosmosCache:ContainerName"];
+            //    cacheOptions.DatabaseName = Configuration["CosmosCache:DatabaseName"];
+            //    cacheOptions.ClientBuilder = new CosmosClientBuilder(Configuration["CosmosCache:ConnectionString"]);
+            //    cacheOptions.CreateIfNotExists = true;
+            //});
+
+            services.AddDistributedSqlServerCache(options =>
+            {
+                //LogSecretVariableValueStartValue(nameof(TokenCacheDbConnStr), TokenCacheDbConnStr);
+
+                options.ConnectionString = TokenCacheDbConnStr;
+                options.SchemaName = "dbo";
+                options.TableName = "TokenCache";
+
+                //def is 2 minutes
+                options.DefaultSlidingExpiration = TimeSpan.FromMinutes(30);
+            });
+
+            #endregion
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CSRO.Server.Auth.Api", Version = "v1" });
             });
+
+            #region DbContext
+
+            var UseSqlLiteDb = Configuration.GetValue<bool>("UseSqlLiteDb");
+
+            services.AddDbContext<UserContext>(options =>
+            {
+                if (UseSqlLiteDb)
+                    options.UseSqlite(Configuration.GetConnectionString("SqlLiteConnString"), x => x.MigrationsAssembly(_namespace));
+                else
+                    options.UseSqlServer(SqlConnString, x => x.MigrationsAssembly(_namespace));
+            });
+
+            #endregion
+
+            #region Repositories
+
+            services.AddScoped(typeof(IRepository<>), typeof(AuthRepository<>));
+
+            #endregion  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +172,7 @@ namespace CSRO.Server.Auth.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
