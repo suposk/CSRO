5bcde3455377097fb842d63caa1c841619b116fa
suposk <jan.supolik@gmail.com>
2021-04-14 14:10:27 +0200

    moved log method

--- src/Client/Blazor/CSRO.Client.Blazor.WebApp/Startup.cs
+++ src/Client/Blazor/CSRO.Client.Blazor.WebApp/Startup.cs
@@ -228,9 +228,9 @@ namespace CSRO.Client.Blazor.WebApp
             //only for client
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
-                .EnableTokenAcquisitionToCallDownstreamApi()    //v1
+                //.EnableTokenAcquisitionToCallDownstreamApi()    //v1
                 //.EnableTokenAcquisitionToCallDownstreamApi(new List<string> { "user.read" })
-                //.EnableTokenAcquisitionToCallDownstreamApi(new List<string> { "user.read", ConstatCsro.Scopes.MANAGEMENT_AZURE_SCOPE })
+                .EnableTokenAcquisitionToCallDownstreamApi(new List<string> { "user.read", "openid", "email", "profile", "offline_access", Configuration.GetValue<string>(Core.ConstatCsro.Scopes.Scope_Auth_Api) })
                 //.EnableTokenAcquisitionToCallDownstreamApi(new List<string> { "https://graph.microsoft.com/.default" })   /v2
                 //.EnableTokenAcquisitionToCallDownstreamApi(new List<string> { "https://graph.microsoft.com/.default", Configuration.GetValue<string>("Scope_Api") })                
                 .AddInMemoryTokenCaches();
