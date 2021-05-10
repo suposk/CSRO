b9000e5a4cf22f009deb0a12184aa2ef4c3de927
suposk <jan.supolik@gmail.com>
2021-05-10 10:47:05 +0200

    FileName added to AppVersion

--- src/Client/CSRO.Client.Services/Dtos/AppVersionDto.cs
+++ src/Client/CSRO.Client.Services/Dtos/AppVersionDto.cs
@@ -21,5 +21,7 @@ namespace CSRO.Client.Services.Dtos
         public string DetailsFormat { get; set; }
 
         public DateTime? ReleasedAt { get; set; }
+
+        public string FileName { get; set; }
     }
 }
--- src/Server/Api/CSRO.Server.Api/Migrations/SqlServerMigrations/20210510084322_FileNameAdded.Designer.cs
+++ src/Server/Api/CSRO.Server.Api/Migrations/SqlServerMigrations/20210510084322_FileNameAdded.Designer.cs
@@ -4,14 +4,16 @@ using CSRO.Server.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSRO.Server.Api.Migrations.SqlServerMigrations
 {
     [DbContext(typeof(AppVersionContext))]
-    partial class AppVersionContextModelSnapshot : ModelSnapshot
+    [Migration("20210510084322_FileNameAdded")]
+    partial class FileNameAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
