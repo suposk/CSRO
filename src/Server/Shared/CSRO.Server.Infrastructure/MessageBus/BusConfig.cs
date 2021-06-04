3db1dce55232741d2cad1233beca4fed1c37b948
suposk <jan.supolik@gmail.com>
2021-06-04 10:18:17 +0200

    global BusConfig

---
+++ src/Server/Shared/CSRO.Server.Infrastructure/MessageBus/BusConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSRO.Server.Infrastructure.MessageBus
+{
+    public enum BusTypeEnum 
+    {
+        /// <summary>
+        /// Not value read
+        /// </summary>
+        None, 
+        /// <summary>
+        /// Azure 
+        /// </summary>
+        AzureServiceBuse, 
+        /// <summary>
+        /// In memory local dev
+        /// </summary>
+        InMemoryMassTransit
+    }
+
+    /// <summary>
+    /// Global Bus config, Disabled bus and type of buss Azure, In memory
+    /// </summary>
+    public class BusConfig
+    {
+        public bool IsBusEnabled { get; set; }
+        public string BusType { get; set; }
+        public BusTypeEnum BusTypeEnum => Enum.TryParse(BusType, true, out BusTypeEnum busTypeEnum) ? busTypeEnum : default(BusTypeEnum);
+
+        public override string ToString()
+        {
+            return $"{nameof(IsBusEnabled)}={IsBusEnabled},{nameof(BusType)}={BusType}";
+        }
+    }
+}
